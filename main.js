95ea47f9b62d8bb17350601d96452ff465d91348
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-03 13:36:28 +0200

    da fare il compito

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+const point1 = new Point(1, 1);
+const point2 = new Point(4, 5);
+
+console.log(point1);
+console.log(point2);
+
+const segment1 = new Segment(point1, point2);
+
+const segment2 = Segment.fromPointsCoordinates(0,1,3,5);
+
+console.log(segment1);
+
+console.log()
+

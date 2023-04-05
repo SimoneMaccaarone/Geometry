dda3fcde5c85ba5bf34527599e269f3537423dc6
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-05 10:20:04 +0200

    trinagolo1e2e3e4

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const point2a = new Point(4, 5);
 
 console.log(point1a);
 console.log(point2a);
-//-------------- SEGMENT--------------------
+//-------------- SEGMENT --------------------
 const segment1 = new Segment(point1a, point2a);
 
 const segment2 = Segment.fromPointsCoordinates(0, 1, 3, 5);
 
 console.log(segment1);
 console.log(segment2);
-//------------- TRIANGLE-------------------
+//------------- TRIANGLE -------------------
 const point1 = new Point(1, 1);
 const point2 = new Point(1, 3);
 const point3 = new Point(1, 3);
@@ -20,8 +20,15 @@ const point4 = new Point(3, 1);
 const point5 = new Point(3, 1);
 const point6 = new Point(1, 1);
 
-const triangle1 = Triangle.fromSegmentCoordinates(point1,point2,point3,point4,point5,point6)
+const triangle1 = Triangle.fromSegmentCoordinates(point1, point2, point3, point4, point5, point6)
 
-// (1, 1, 1, 3, 1, 3, 3, 1, 3, 1, 1, 1)
+console.log('L AREA del Triangolo è: ',triangle1.area);
 
-console.log(triangle1.area);
+//------------- TRIANGLE2 -----------------
+
+const pointA = new Point(0, 0)
+const pointB = new Point(0, 3)
+const pointC = new Point(4, 0)
+
+const triangle2 = new Triangle2(pointA, pointB, pointC);
+console.log('Il PERIMETRO del Triangolo2 è: ',triangle2.calculatePerimeter());
--- triangle.js
+++ triangle.js
@@ -1,10 +1,10 @@
 class Triangle {
 
-    // creazione generale del triangolo. SENZA ISTRUZIONI E POSIZIONI DEI SEGMENTI
+    // creazione generale del triangolo. SENZA ISTRUZIONI E PaicOSIZIONI DEI SEGMENTI
     constructor(segmentA, segmentB, segmentC) {
-        this.segmentA = segmentA
-        this.segmentB = segmentB
-        this.segmentC = segmentC
+        this.segmentA = segmentA;
+        this.segmentB = segmentB;
+        this.segmentC = segmentC;
     }
 
     static fromSegmentCoordinates(xA, yA, xB, yB, xC, yC) {
@@ -20,12 +20,82 @@ class Triangle {
         const resultA = sP - this.segmentA.length;
         const resultB = sP - this.segmentB.length;
         const resultC = sP - this.segmentC.length;
-        console.log(perimetro);
+        console.log('Il PERIMETRO del Triangolo è: ', perimetro);
 
-        const resultPippo = sP * resultA * resultB * resultC
-        const resultFinale = Math.sqrt((resultPippo))
-        return resultFinale
+        const resultPippo = sP * resultA * resultB * resultC;
+        const resultFinale = Math.sqrt((resultPippo));
+        return resultFinale;
+    }
+}
 
+class Triangle2 {
+
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+
+        this.segmentAB = new Segment(this.pointA, this.pointB);
+        this.segmentBC = new Segment(this.pointB, this.pointC);
+        this.segmentCA = new Segment(this.pointC, this.pointA);
     }
 
+    calculatePerimeter() {
+        return this.segmentAB.length, this.segmentBC.length, this.segmentCA.length;
+    }
+
+    calculateSemiPerimeter() {
+        return this.calculatePerimeter() / 2;
+    }
+
+    calculateArea() {
+        const semiPerimeterMinusSegmentAB = this.calculatePerimeter() - this.segmentAB.length
+        const semiPerimeterMinusSegmentBC = this.calculatePerimeter() - this.segmentBC.length
+        const semiPerimeterMinusSegmentCA = this.calculatePerimeter() - this.segmentCA.length
+
+        const product = this.calculateSemiPerimeter() *
+                        semiPerimeterMinusSegmentAB *
+                        semiPerimeterMinusSegmentBC *
+                        semiPerimeterMinusSegmentCA;
+        const area = Math.sqrt(product);
+        return area;
+
+    }
+}
+
+class Triangle3 {
+
+    constructor(pointA, pointB, pointC) {
+        this.segmentAB = new Segment(pointA, pointB);
+        this.segmentBC = new Segment(pointB, pointC);
+        this.segmentCA = new Segment(pointC, pointA);
+    }
+    get pointA() {
+        return this.segmentAB.pointA;
+    }
+    get pointB() {
+        return this.segmentAB.pointB;
+    }
+    get pointC() {
+        return this.segmentBC.pointC;
+    }
+}
+
+class Triangle4 {
+
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA
+        this.pointB = pointB
+        this.pointC = pointC
+
+    }
+    get pointAB() {
+        return new Segment(this.pointA, this.pointB);
+    }
+    get pointBC() {
+        return new Segment(this.pointB, this.pointC);
+    }
+    get pointCA() {
+        return new Segment(this.pointC, this.pointA);
+    }
 }
